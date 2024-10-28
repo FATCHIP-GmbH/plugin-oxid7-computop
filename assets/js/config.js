56ffde7bf1e8cac1726ab2f4f576d626e64b0067
Danny <danny.zimmer@maexware-solutions.de>
2024-10-28 14:45:32 +0100

    comments

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
     const idealSelect = document.getElementById("idealDirektOderUeberSofort");
     const bankSection = document.getElementById("bankSection");
 
-    // Funktion zum Anzeigen/Ausblenden des Bankbereichs
     function toggleBankSection() {
         if (idealSelect.value === "PPRO") {
             bankSection.style.display = "none";
@@ -14,9 +13,7 @@ $(document).ready(function(){
         }
     }
 
-    // Initiale Ausführung, um den aktuellen Status zu setzen
     toggleBankSection();
 
-    // Event Listener für Änderungen in der Selectbox
     idealSelect.addEventListener("change", toggleBankSection);
 });
