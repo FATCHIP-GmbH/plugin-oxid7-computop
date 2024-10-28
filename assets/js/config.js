68378192aa9938bb6b371d962361daa1ed0fa47b
Danny <danny.zimmer@maexware-solutions.de>
2024-10-28 13:27:35 +0100

    remove banking if ppro is selected

--- assets/js/config.js
+++ assets/js/config.js
@@ -2,4 +2,21 @@ $(document).ready(function(){
     $('[data-toggle="popover"]').popover({
         html: true // Enable HTML rendering in popover content
     });
+    const idealSelect = document.getElementById("idealDirektOderUeberSofort");
+    const bankSection = document.getElementById("bankSection");
+
+    // Funktion zum Anzeigen/Ausblenden des Bankbereichs
+    function toggleBankSection() {
+        if (idealSelect.value === "PPRO") {
+            bankSection.style.display = "none";
+        } else {
+            bankSection.style.display = "block";
+        }
+    }
+
+    // Initiale Ausführung, um den aktuellen Status zu setzen
+    toggleBankSection();
+
+    // Event Listener für Änderungen in der Selectbox
+    idealSelect.addEventListener("change", toggleBankSection);
 });
